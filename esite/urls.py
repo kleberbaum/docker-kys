f2c7ba8e6648561a945c46fec71be1826b8dd8ff
kleberbaum <kleberbaum@erebos.xyz>
2018-11-26 10:01:50 +0100

    remove some urls

--- esite/urls.py
+++ esite/urls.py
@@ -1,36 +1,53 @@
-from django.conf import settings
 from django.conf.urls import include, url
+from django.conf import settings
 from django.contrib import admin
 
 from wagtail.admin import urls as wagtailadmin_urls
-from wagtail.core import urls as wagtail_urls
 from wagtail.documents import urls as wagtaildocs_urls
+from wagtail.contrib.sitemaps.views import sitemap
+from wagtail.core import urls as wagtail_urls
 
 from esite.search import views as search_views
 
+from django.views.decorators.csrf import csrf_exempt
+from graphene_django.views import GraphQLView
+
 urlpatterns = [
-    # url(r'^django-admin/', admin.site.urls),
+    #url(r'^django-admin/', admin.site.urls),
 
     url(r'^admin/', include(wagtailadmin_urls)),
     url(r'^documents/', include(wagtaildocs_urls)),
 
-    url(r'^search/$', search_views.search, name='search'),
+    #url(r'^search/$', search_views.search, name='search'),
 
-    # For anything not caught by a more specific rule above, hand over to
-    # Wagtail's page serving mechanism. This should be the last pattern in
-    # the list:
-    url(r'', include(wagtail_urls)),
-
-    # Alternatively, if you want Wagtail pages to be served from a subpath
-    # of your site, rather than the site root:
-    #    url(r'^pages/', include(wagtail_urls)),
+    #url('^sitemap\.xml$', sitemap),
+    #url(r'^api/v2/', api_router.urls),
 ]
 
 
 if settings.DEBUG:
     from django.conf.urls.static import static
     from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+    from django.views.generic import TemplateView
+    from django.views.generic.base import RedirectView
 
     # Serve static and media files from development server
     urlpatterns += staticfiles_urlpatterns()
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += [
+        url(
+            r'^favicon\.ico$', RedirectView.as_view(
+                url=settings.STATIC_URL + 'img/bread-favicon.ico'
+                )
+            )
+    ]
+
+    # Add views for testing 404 and 500 templates
+    urlpatterns += [
+        url(r'^test404/$', TemplateView.as_view(template_name='404.html')),
+        url(r'^test500/$', TemplateView.as_view(template_name='500.html')),
+    ]
+
+urlpatterns += [
+    url(r'', include(wagtail_urls)),
+]
